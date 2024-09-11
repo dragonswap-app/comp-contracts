e2ad44dd793b0bbf2198076f4dc6bd186e6ce502
markolazic01 <markolazic01@gmail.com>
2024-09-11 19:25:06 +0200

    chore: Adapt existing files

--- src/Competition.sol
+++ src/Competition.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
-contract Counter {
+contract Competition {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
---
+++ test/Competition.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Competition} from "../src/Competition.sol";
+
+contract CompetitionTest is Test {
+    Competition public competition;
+
+    function setUp() public {
+        competition = new Competition();
+        competition.setNumber(0);
+    }
+
+    function test_Increment() public {
+        competition.increment();
+        assertEq(competition.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        competition.setNumber(x);
+        assertEq(competition.number(), x);
+    }
+}
