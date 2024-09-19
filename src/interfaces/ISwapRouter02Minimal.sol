0505aea8dbe570eb33b9eaeb82a43bae2b619681
markolazic01 <markolazic01@gmail.com>
2024-09-19 13:28:02 +0200

    chore: Reform interfaces / remove unnecessary stuff

--- src/interfaces/ISwapRouter02Minimal.sol
+++ src/interfaces/ISwapRouter02Minimal.sol
@@ -2,8 +2,36 @@
 pragma solidity 0.8.25;
 
 /// @title Router token swapping functionality
+/// @notice Functions for swapping tokens via Dragonswap V1
+interface IV1SwapRouter {
+    /// @notice Swaps `amountIn` of one token for as much as possible of another token
+    /// @dev Setting `amountIn` to 0 will cause the contract to look up its own balance,
+    /// and swap the entire amount, enabling contracts to send tokens before calling this function.
+    /// @param amountIn The amount of token to swap
+    /// @param amountOutMin The minimum amount of output that must be received
+    /// @param path The ordered list of tokens to swap through
+    /// @param to The recipient address
+    /// @return amountOut The amount of the received token
+    function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to)
+        external
+        payable
+        returns (uint256 amountOut);
+
+    /// @notice Swaps as little as possible of one token for an exact amount of another token
+    /// @param amountOut The amount of token to swap for
+    /// @param amountInMax The maximum amount of input that the caller will pay
+    /// @param path The ordered list of tokens to swap through
+    /// @param to The recipient address
+    /// @return amountIn The amount of token to pay
+    function swapTokensForExactTokens(uint256 amountOut, uint256 amountInMax, address[] calldata path, address to)
+        external
+        payable
+        returns (uint256 amountIn);
+}
+
+/// @title Router token swapping functionality
 /// @notice Functions for swapping tokens via Dragonswap V2
-interface IV2SwapRouterNoCallback {
+interface IV2SwapRouter {
     struct ExactInputSingleParams {
         address tokenIn;
         address tokenOut;
@@ -64,3 +92,6 @@ interface IV2SwapRouterNoCallback {
     /// @return amountIn The amount of the input token
     function exactOutput(ExactOutputParams calldata params) external payable returns (uint256 amountIn);
 }
+
+/// @title Router token swapping functionality
+interface ISwapRouter02Minimal is IV1SwapRouter, IV2SwapRouter {}
