d5e3297ea6fda1a25c656627048cd9bd9cb089a2
markolazic01 <markolazic01@gmail.com>
2024-09-24 13:15:08 +0200

    wip tests

--- test/Competition.t.sol
+++ test/Competition.t.sol
@@ -2,23 +2,44 @@
 pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-
+import {ISwapRouter02Minimal} from "../src/interfaces/ISwapRouter02Minimal.sol";
 import {Competition} from "../src/Competition.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts@5.0.2/token/ERC20/utils/SafeERC20.sol";
 
 contract CompetitionTest is Test {
     Competition public competition;
+    address[] public swapTokens;
 
-    address public constant DS_ROUTER = 0x11DA6463D6Cb5a03411Dbf5ab6f6bc3997Ac7428;
+    ISwapRouter02Minimal public constant DS_ROUTER = ISwapRouter02Minimal(0x11DA6463D6Cb5a03411Dbf5ab6f6bc3997Ac7428);
+    // @dev USDC and USDT are both native sei tokens which means they include precompile interaction which breaks the tests
     address public constant USDC = 0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1;
     address public constant USDT = 0xB75D0B03c06A926e488e2659DF1A861F860bD3d1;
     address payable public constant WSEI = payable(0xE30feDd158A2e3b13e9badaeABaFc5516e95e8C7);
     string public constant URL = "https://evm-rpc.sei-apis.com";
-    address[] public swapTokens;
 
     function setUp() public {
         vm.createSelectFork(URL);
+        competition =
+            new Competition();
 
-        competition = new Competition(block.timestamp, block.timestamp + 1 days, DS_ROUTER, USDC, USDT, swapTokens);
+        competition.initialize(block.timestamp, block.timestamp + 1 days, address(DS_ROUTER), USDC, USDT, swapTokens);
+
+        (bool success,) = WSEI.call{value: 100e18}("");
+
+        address[] memory wseiUsdcPath = new address[](2);
+        wseiUsdcPath[0] = WSEI;
+        wseiUsdcPath[1] = USDC;
+
+        address[] memory wseiUsdtPath = new address[](2);
+        wseiUsdtPath[0] = WSEI;
+        wseiUsdtPath[1] = USDT;
+
+        IERC20(WSEI).approve(address(DS_ROUTER), 100e18);
+        DS_ROUTER.swapExactTokensForTokens(50e18, 0, wseiUsdcPath, address(this));
+        DS_ROUTER.swapExactTokensForTokens(50e18, 0, wseiUsdtPath, address(this));
+
+        console.log(IERC20(USDC).balanceOf(address(this)));
+        console.log(IERC20(USDT).balanceOf(address(this)));
     }
 
     function test_depositViaFunction() public {
